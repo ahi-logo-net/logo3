62fa68c5ab2c9fdcb03ad59df2ab30838d11fc23
ahi-logo-net <sr0210046@gmail.com>
2025-02-14 00:18:30 +0900

    2/14 - 1

--- logomake.js
+++ logomake.js
@@ -1,3 +1,9 @@
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function(event){
+    event.preventDefault();
+    var letter = document.getElementById('letter').value;
+    document.getElementById('bun').textContent = `${letter}`;
+})
 letter.addEventListener('change', function(event){
     event.preventDefault();
     var letter = document.getElementById('letter').value;
